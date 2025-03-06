6fe2a1b4eeb13179f1b03471403970257602dd9e
danyal sameer khalife <141676015+Danyalsk@users.noreply.github.com>
2025-03-07 03:39:58 +0530

    Remove authentication middleware from user creation route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,18 +1,13 @@
 import express from "express";
 const router = express.Router();
 import userController from "../controllers/userController.js";
-import authMiddleware from "../middlewares/authMiddleware.js";
+// import authMiddleware from "../middlewares/authMiddleware.js";
 
 router.post("/login", userController.login);
 router.get("/users", userController.getUsers);
-router.post(
-  "/create-user",
-  userController.createUser,
-  authMiddleware,
-  (req, res) => {
-    res.json({ message: "User created successfully!" });
-  }
-);
+router.post("/create-user", userController.createUser, (req, res) => {
+  res.json({ message: "User created successfully!" });
+});
 
 router.patch("/edit-user/:id", userController.editUser);
 
