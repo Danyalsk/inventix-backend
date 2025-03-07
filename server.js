30e98a333be0e68fb3de4e9189f92251d620bc03
danyal sameer khalife <141676015+Danyalsk@users.noreply.github.com>
2025-03-07 08:28:19 +0530

    Update CORS configuration to allow requests from any origin

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import UserRouter from "./src/routes/userRouter.js";
 const app = express();
 const port = 4040;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/v1", routes);
